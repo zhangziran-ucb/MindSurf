53848720b7124204a458b02d9a64efb6d20f8246
a455bcd9 <kelemonde.aibo@club-internet.fr>
2015-10-13 18:24:48 -0700

    Adding timestamp

--- python-emotiv-master/emotiv/create.py
+++ python-emotiv-master/emotiv/create.py
@@ -418,7 +418,7 @@ def main():
     t = datetime.now()
     filename = t.strftime('%Y%m%d%H%M')
     target = open('../../data/emotiv/' + filename, 'w')
-    target.write("Gyro(x) Gyro(y) F3 FC5 AF3 F7 T7 P7 O1 O2 P8 T8 F8 AF4 FC6 F4\n")
+    target.write("H:M:S.f Gyro(x) Gyro(y) F3 FC5 AF3 F7 T7 P7 O1 O2 P8 T8 F8 AF4 FC6 F4\n")
     while 1:
         try:
             data = e.get_sample()
